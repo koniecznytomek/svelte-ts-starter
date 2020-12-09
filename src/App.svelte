a3889031f9ae94ca636895e42c422d9c004c5b76
koniecznytomek <tomek@tomekkonieczny.com>
2020-12-09 12:43:53 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+	import Test from './components/Test/Test.svelte';
+</script>
+
+<main>
+	<h1>Hello</h1>
+	<Test/>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 100%;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+</style>
---
+++ src/components/Test/Test.svelte
@@ -0,0 +1,7 @@
+<style src="./Test.scss"></style>
+
+<script type="ts">
+   let a: string = 'test';
+</script>
+
+<h1>This is {a} component.</h1>
